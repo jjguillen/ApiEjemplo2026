1da8f7684e64c4c5c71adb0a2ae83f07dd816266
jjguillen <jjavierguillen@gmail.com>
2026-01-15 19:59:48 +0100

    Empezando la clase para continuar API

--- src/main/java/com/jaroso/apiejemplo2026/controllers/TaskController.java
+++ src/main/java/com/jaroso/apiejemplo2026/controllers/TaskController.java
@@ -3,10 +3,9 @@ package com.jaroso.apiejemplo2026.controllers;
 import com.jaroso.apiejemplo2026.entities.Task;
 import com.jaroso.apiejemplo2026.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,6 +34,11 @@ public class TaskController {
         return task.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @PostMapping("/tasks")
+    public ResponseEntity<Task> createTask(@RequestBody Task task){
+        //return ResponseEntity.ok(taskService.saveTask(task));
+        return ResponseEntity.status(HttpStatus.CREATED).body(taskService.saveTask(task));
+    }
 
 
 
